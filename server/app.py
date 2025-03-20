561c9d40c66ffe0636bc5f874bf7a6affe5b8023
Usman Mahmood <usmanmahmood254@gmail.com>
2025-03-20 00:20:04 -0600

    update made to backend

--- server/app.py
+++ server/app.py
@@ -645,4 +645,4 @@ def weather():
     return jsonify({"location": latest_location, "weather": latest_weather})
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
